60a8050301de51e73326d3124edfd66ea779ca84
Tomas Enarsson <tomas.enarsson@outlook.com>
2022-02-06 14:27:44 +0100

    Add meta for utils

---
+++ src/utils.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name Utils
+// @namespace Tomas
+// @match *://*/*
+// @grant GM_deleteValue
+// @grant GM_getValue
+// @grant GM_listValues
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant GM_setValue
+// @grant GM_xmlhttpRequest
+// ==/UserScript==
