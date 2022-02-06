12ab9d961d3f6c9981e030ca9480864d49c64ae0
Tomas Enarsson <tomas.enarsson@outlook.com>
2022-02-06 14:27:20 +0100

    Move require to head

--- src/footer.js
+++
@@ -1,7 +0,0 @@
-(function main() {
-  "use strict";
-  const { GM } = require("./gm");
-  const { utils } = require("./utils");
-  GM.unsafeWindow._utils = Object.freeze(utils);
-  console.log("Utils loaded.");
-})();
---
+++ src/head.js
@@ -0,0 +1,21 @@
+function require(modName) {
+    const { modMap } = register;
+    if (!modMap.has(modName)) throw Error(`Module "${modName}" is not registered.`);
+    const mod = modMap.get(modName);
+    if (mod._exported) return mod.exports;
+    mod.fn.call(undefined, mod.exports, mod);
+    mod._exported = true;
+    return mod.exports;
+}
+
+function register(modName, modFn) {
+    const { modMap } = register;
+    modMap.set(modName, {
+        name: modName,
+        fn: modFn,
+        exports: {},
+        exported: false,
+    });
+}
+
+register.modMap = new Map();
