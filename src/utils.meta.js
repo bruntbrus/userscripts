db5d974e989b6d253e77732ad6efb8269c9670f7
Tomas Enarsson <tomas.enarsson@outlook.com>
2022-02-13 12:05:28 +0100

    Grant whole GM api

--- src/utils.meta.js
+++ src/utils.meta.js
@@ -3,9 +3,12 @@
 // @namespace Tomas
 // @match *://*/*
 // @grant GM_deleteValue
+// @grant GM_getResourceURL
 // @grant GM_getValue
 // @grant GM_listValues
+// @grant GM_notification
 // @grant GM_openInTab
+// @grant GM_registerMenuCommand
 // @grant GM_setClipboard
 // @grant GM_setValue
 // @grant GM_xmlhttpRequest
